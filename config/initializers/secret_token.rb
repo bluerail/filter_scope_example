95cd4e3fac3c862fe5847080da341aeaa0cefa55
Martijn Storck <martijn@lico.nl>
2012-08-07 14:12:41 +0200

    Initial commit: lege Rails applicatie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilterScopeExample::Application.config.secret_token = '5249fc239f6f9e56068aef048c8013165163102cbfd069d4b30dac1bde48d2871aca4c7dea693fd53fcb5c3327eb6bb7733329e7f6cdb9ce913838ce2a871746'
